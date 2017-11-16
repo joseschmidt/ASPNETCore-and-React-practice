41f535ae161fd42c580fcac5bb6a67ba4246e218
Kelvin Ferreira <k30v1n@users.noreply.github.com>
2017-11-16 11:47:10 -0200

    Adding Web Project and Example REST Api

---
+++ ASPNETCore-React/ASPNETCore-React.Web/Controllers/Example/ExampleController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ASPNETCore_React.Web.Controllers.Example
+{
+    [Route("api/[controller]")]
+    public class ExampleController : Controller
+    {
+        // GET api/example
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/example/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value" + id;
+        }
+
+        // POST api/example
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/example/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/example/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

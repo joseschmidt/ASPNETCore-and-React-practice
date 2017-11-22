86ed8d973c305237711ec99b61f29d0d54a47fb0
Kelvin Ferreira <k30v1n@users.noreply.github.com>
2017-11-22 17:16:43 -0200

    Index view now is a Menu to the practices. Created the Example view to have the basic utilization of React and Api. Created the ReturnToIndex partial view to centralize the link to get back to main menu (index)

--- ASPNETCore-React/ASPNETCore-React.Web/Controllers/MVC/HomeController.cs
+++ ASPNETCore-React/ASPNETCore-React.Web/Controllers/MVC/HomeController.cs
@@ -8,7 +8,25 @@ namespace ASPNETCore_React.Web.Controllers.MVC
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Project's main page
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// A partial view to render controls to return to main page
+        /// </summary>
+        /// <returns></returns>
+        public PartialViewResult ReturnToIndex()
+        {
+            return PartialView();
+        }
+
+        public IActionResult Example()
         {
             return View();
         }

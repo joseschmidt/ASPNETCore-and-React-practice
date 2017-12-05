214f55c253f26b3054bebd6443d68ee54e0999cb
Kelvin Ferreira <k30v1n@users.noreply.github.com>
2017-12-05 09:39:18 -0200

    DDD and unity testing structure
    - DDD Structure added on the project
    - Testing structure added on the project and was implemented a unity testing for HomeController

---
+++ src/ASPNETCore-React.Test/Web/MVC/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using ASPNETCore_React.Web.Controllers.MVC;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ASPNETCore_React.Test.Web.MVC
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_ReturnsAViewResult_Empty()
+        {
+            //Arrange
+            var controller = new HomeController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.Model);
+        }
+    }
+}

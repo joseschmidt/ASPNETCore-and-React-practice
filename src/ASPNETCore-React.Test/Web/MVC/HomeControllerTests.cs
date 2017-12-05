8fb9fd196c41ed8a387d4aadee2b0efeeae0ba29
Kelvin Ferreira <k30v1n@users.noreply.github.com>
2017-12-05 09:44:57 -0200

    MVC Controller Tests
    - Unity testing of MVC Controllers

--- src/ASPNETCore-React.Test/Web/MVC/HomeControllerTests.cs
+++ src/ASPNETCore-React.Test/Web/MVC/HomeControllerTests.cs
@@ -22,5 +22,33 @@ namespace ASPNETCore_React.Test.Web.MVC
             var viewResult = Assert.IsType<ViewResult>(result);
             Assert.Null(viewResult.Model);
         }
+
+        [Fact]
+        public void ReturnToIndex_ReturnsPartialView_Empty()
+        {
+            //Arrange
+            var controller = new HomeController();
+
+            //Act
+            var result = controller.ReturnToIndex();
+
+            //Assert
+            var partialView = Assert.IsType<PartialViewResult>(result);
+            Assert.Null(partialView.Model);
+        }
+
+        [Fact]
+        public void Example_ReturnsAViewResult_Empty()
+        {
+            //Arrange
+            var controller = new HomeController();
+
+            //Act
+            var result = controller.Example();
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.Model);
+        }
     }
 }
